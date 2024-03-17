5a3a09ac28d6282fe31b01e5e23233a5fd66d3df
ejazkhan10 <ijaz.earner786@gmail.com>
2024-03-17 15:54:31 +0430

    Refactored reusable api client

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,9 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import apiClient, { FetchResponse } from "../services/api-client";
+import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
 
-
+const apiClient = new APIClient<Game>("/games")
 export interface Game {
   id: number;
   name: string;
@@ -18,14 +18,13 @@ const useGames = (gameQuery: GameQuery) => {
 
   return useQuery({
     queryKey: gameQuery ? ["games", gameQuery] : ["games"],
-    queryFn: () => apiClient.get<FetchResponse<Game>>("/games", {
-      params: {
-        games: gameQuery.genre?.id,
-        parent_platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText
-      }
-    }).then(res => res.data),
+    queryFn: () => apiClient.getAll({params: {
+      genres: gameQuery.genre?.id,
+              platforms: gameQuery.platform?.id,
+              ordering: gameQuery.sortOrder,
+              search: gameQuery.searchText
+    }}),
+
     staleTime: 60*60 * 1000,
   })
 }
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
-export default axios.create({
+const axiosInstance =  axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "941ae3f8b0bb43d589db870343179c43",
@@ -12,3 +12,24 @@ export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
+
+
+
+
+class APIClient<T> {
+  endpoint: string;
+  params?: Object
+
+
+  constructor(endpoint: string, params = {}) {
+    this.endpoint = endpoint;
+    this.params = params
+  }
+
+  getAll =(config?: AxiosRequestConfig) => {
+    return axiosInstance.get<FetchResponse<T>>(this.endpoint, config).then(res => res.data)
+  }
+}
+
+
+export default APIClient;
