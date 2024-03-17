fd92a8656b4e71293a416ebcfe1f5933edc7f8d1
ejazkhan10 <ijaz.earner786@gmail.com>
2024-03-17 19:34:02 +0430

    on scroll pagination done

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,23 +1,34 @@
-import { Button, SimpleGrid, Text } from "@chakra-ui/react";
+import { Button, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import { GameQuery } from "../App";
 import useGames, { Platform } from "../hooks/useGames";
 import { Genre } from "../hooks/useGenres";
 import GameCard from "./GameCard";
 import GameCardContainer from "./GameCardContainer";
 import GameCardSkeleton from "./GameCardSkeleton";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const { data, error, isLoading, fetchNextPage, isFetchingNextPage } = useGames(gameQuery);
+  const { data, error, isLoading, fetchNextPage, isFetchingNextPage, hasNextPage } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
+
+  const fetchedGamesCount = data?.pages.reduce((a, b) => a + b.results.length, 0) || 0;
 
   if (error) return <Text>{error}</Text>;
 
   return (
     <>
+
+
+  <InfiniteScroll
+  dataLength={fetchedGamesCount}
+  next={() => fetchNextPage()}
+  hasMore={!!hasNextPage}
+  loader={<Spinner/>}
+  >
     <SimpleGrid
       columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
       padding="10px"
@@ -29,6 +40,7 @@ const GameGrid = ({ gameQuery }: Props) => {
             <GameCardSkeleton />
           </GameCardContainer>
         ))}
+
       {data?.pages.map((page) => (
         page.results.map((game) =>  (
         <GameCardContainer key={game.id}>
@@ -37,12 +49,13 @@ const GameGrid = ({ gameQuery }: Props) => {
         )
       )))}
     </SimpleGrid>
+      </InfiniteScroll>
 
-    <Button className="btn-primary" onClick={() => fetchNextPage()}>
+    {/* <Button className="btn-primary" onClick={() => fetchNextPage()}>
 
       {isFetchingNextPage ? "Loading" : "Load More"}
 
-      </Button>
+      </Button> */}
     </>
   );
 };
